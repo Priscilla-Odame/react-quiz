a6c79266470b2cb7563cf6e7eaa7f49cda03d5ff
Priscilla Odame <priscillaodame5@gmail.com>
2020-08-29 03:04:27 +0000

    Work on Total

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
 
 
-// state data for 3 counters
+// // state data for 3 counters
 // const data = [
 //   { id: 1, value: 0 },
 //   { id: 2, value: 0 },
@@ -11,76 +10,93 @@ import ReactDOM from "react-dom";
 
 // Counter Component
 class Counter extends Component {
-  constructor() {
+  constructor(){
     super();
-    count = 0;
+    this.state = {
+      count: 0
+    };
   }
-  
   onIncrement = () =>{
     this.setState({
       count: this.state.count + 1
     });
- 
+
   } ;
   onDecrement = () =>{
     this.setState({
       count: this.state.count - 1
     });
   } ;
+  
+ 
   render() {
-    const { value } = this.props;
+    let { value } = this.props;
     let {count} = this.state;
-    value= count;
+    value = count;
     return (
       <div className="counter">
         <b>{value}</b>
         <div className="counter-controls">
-          <button className="button is-danger is-small" onClick = {this.onDecrement}>-</button>
-          <button className="button is-success is-small" onClick= {this.onIncrement}>+</button>
+          <button className="button is-danger is-small" onClick={this.onDecrement}>-</button>
+          <button className="button is-success is-small" onClick={this.onIncrement}>+</button>
         </div>
+      </div>
+    );
+  }
+}
+
+
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      data: [
+        { id: 1, value: 0 },
+        { id: 2, value: 0 },
+        { id: 3, value: 0 },
+        { id: 4, value: 0 },
+      ]
+  }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.data.map((counter) => (
+          <Counter key={counter.id} value={counter.value} />
+        ))}
+        <Total />
       </div>
     );
   }
 }
 
 class Total extends Component {
-  totalCount = e => {
-    this.props();
-  };
-
+  constructor(){
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+  
+  totalCount = () =>{
+    this.setState({
+      count: this.state.count
+    });
+  } ;
+  
   render() {
-    const { value } = this.props;
+    let { value } = this.props;
+    let {count} = this.state;
+    value = count;
     return (
+      
       <div className="totalCount">
-        <b>{value}</b>
         <div className="total-count">
-          <button className="totalCount" onClick={`The total Count is: ${getElementById('1', '2', '3', '4')}`}>Total Count</button>
+          <b>{value}</b>
+         <button className="totalCount" onClick={this.totalCount}>Total Count</button>     
         </div>
-      </div>
-    );
-  }
-}
-
-class App extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state={ 
-      data: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      {id: 4, value: 0},
-    ]
-  }
-
-  }
-
-  render() {
-    return (
-      <div>
-        {data.map((counter) => (
-          <Counter key={counter.id} value={counter.value} />
-        ))}
       </div>
     );
   }
